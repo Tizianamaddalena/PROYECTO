dfe4a57d923a40ef777d6b191fab0cf57ee59117
Nico <riccitelline@gmail.com>
2019-11-01 13:58:44 -0300

    banda de cosas

--- js/js.js
+++ js/js.js
@@ -1,10 +1,21 @@
+window.onload = function (){
+
 fetch("https://api.themoviedb.org/3/movie/550?api_key=60be6317b0012697cb25d6ce427de920")
-.then(function(response){
-  return response.json();
+.then(function(response) {
+ return response.json()
 })
-.then(function(data){
+.then(function(information) {
+ console.log(information.data);
+var nombre= information.data.title;
+var url = information.data.image_url;
+
+var titulo=document.querySelector("#titulo");
+titulo.innerHTML=nombre;
+var imagen=document.querySelector(".imagen")
+imagen.innerHTML =  ' <img src="'+url+'" alt="">'
 
 })
-.catch(function(error)
-{console.log("El error fue: " + error);
+.catch(function(error) {
+ console.log("Error: " + error);
 })
+}
