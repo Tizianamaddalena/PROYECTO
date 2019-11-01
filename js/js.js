bbe8ea677b035ef32e6cda640975e32776e33753
Nico <riccitelline@gmail.com>
2019-11-01 14:11:12 -0300

    JavaScript

--- js/js.js
+++ js/js.js
@@ -1,21 +1,29 @@
 window.onload = function (){
-
-fetch("https://api.themoviedb.org/3/movie/550?api_key=60be6317b0012697cb25d6ce427de920")
+  //
+var api_key = "60be6317b0012697cb25d6ce427de920"
+fetch("https://api.themoviedb.org/3/tv/popular?api_key="+api_key+"&language=en-US&page=1")
 .then(function(response) {
  return response.json()
 })
 .then(function(information) {
- console.log(information.data);
-var nombre= information.data.title;
-var url = information.data.image_url;
-
-var titulo=document.querySelector("#titulo");
-titulo.innerHTML=nombre;
-var imagen=document.querySelector(".imagen")
-imagen.innerHTML =  ' <img src="'+url+'" alt="">'
+console.log(information.results);
+  for (var i = 0; i < information.results.length; i++) {
+    console.log(information.results[i].name)
+  }
+ // console.log(information.results[0].name);
+// var nombre= information.data.title;
+// var url = information.data.image_url;
+//
+// var titulo=document.querySelector("#titulo");
+// titulo.innerHTML=nombre;
+// var imagen=document.querySelector(".imagen")
+// imagen.innerHTML =  ' <img src="'+url+'" alt="">'
 
 })
 .catch(function(error) {
  console.log("Error: " + error);
 })
+
+
+
 }
