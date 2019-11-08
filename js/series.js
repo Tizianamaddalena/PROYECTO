2863b66389f36c0e1984e391f5fd510cbd778664
Tiziana <tizimb@gmail.com>
2019-11-08 10:52:45 -0300

    agrego algo se series.html

---
+++ js/series.js
@@ -0,0 +1,20 @@
+window.addEventListener('load',function(){
+  //
+var api_key = "60be6317b0012697cb25d6ce427de920"
+fetch("")
+
+.then(function(response) {
+ return response.json()
+})
+
+.then(function(information) {
+
+})
+
+.catch(function(error) {
+ console.log("Error: " + error);
+})
+
+
+
+})
