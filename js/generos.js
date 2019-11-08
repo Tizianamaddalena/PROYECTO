5e54403d180887367134e9028cd7c023b1150967
rosariorodriguezm <rrodriguezmontenegro@udesa.edu.ar>
2019-11-08 14:06:05 -0300

    lista de generos

--- js/generos.js
+++ js/generos.js
@@ -1,4 +1,4 @@
-window.addEventListener('load',function()){
+window.addEventListener('load',function(){
 //
 var listaGeneros = document.querySelector('#dropdown-generos');
 
@@ -6,22 +6,20 @@ var api_key = "60be6317b0012697cb25d6ce427de920"
 fetch("https://api.themoviedb.org/3/genre/tv/list?api_key="+api_key+"&language=es-AR&page=1")
 .then(function(response) {
  return response.json()
-
 })
 .then(function(informacion){
-   var generos = informacion.results;
-
+   var generos = informacion.genres;
+   console.log("===========");
+   console.log(generos);
+   console.log("===========");
    for (var i = 0; i <= 15; i++) {
      listaGeneros.innerHTML += `
-     <li> <button type="button" class="opciones" id="generos">GENEROS</button>
-     <div uk-dropdown="pos: bottom-justify">
-     <ul class="uk-nav uk-dropdown-nav" id="dropdown-generos">${generos[i].title}</ul>
-     </div>
-     </li>
-    `;
+        <li><a href="#">${generos[i].name}</a></li>
+    `
+    }
   })
-  
+
 .catch(function(error){
   console.log("Error: " + error);
 })
-}
+})
