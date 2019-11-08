5758116237338a643f1451f1ae2c2bd907b9e4de
rosariorodriguezm <rrodriguezmontenegro@udesa.edu.ar>
2019-11-08 09:46:35 -0300

    DESCARGE UIKIT Y API EN ESPAÑOL

--- js/masPopulares.js
+++ js/masPopulares.js
@@ -1,7 +1,7 @@
 window.addEventListener('load',function(){
   //
 var api_key = "60be6317b0012697cb25d6ce427de920"
-fetch("https://api.themoviedb.org/3/tv/popular?api_key="+api_key+"&language=en-US&page=1")
+fetch("https://api.themoviedb.org/3/tv/popular?api_key="+api_key+"&language=es-AR&page=1")
 .then(function(response) {
  return response.json()
 })
