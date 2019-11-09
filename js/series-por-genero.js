d569b25cee0c618e68fc569f5fccea02436a613b
rosariorodriguezm <rrodriguezmontenegro@udesa.edu.ar>
2019-11-09 13:21:47 -0300

    correccion header e intento2

--- js/series-por-genero.js
+++ js/series-por-genero.js
@@ -1,33 +1,25 @@
 window.addEventListener('load', function () {
-  var urlParams = new URLSearchParams(location.search);
+var urlParams = new URLSearchParams(location.search);
 
-})
+var idGenero = urlParams.get('genre_ids');
+var nombreGenero = document.querySelector('#titulo-genero')
+
 
 var api_key = "60be6317b0012697cb25d6ce427de920"
-fetch("https://api.themoviedb.org/3/discover/tv?api_key="+api_key+"&with_genres=18")
+fetch("https://api.themoviedb.org/3/discover/tv?api_key="+api_key+"&sort_by=popularity.desc&page=1&with_genres=18")
 .then(function(response){
   return response.json()
 })
-.then(function(informacion) {
-  console.log(informacion);
-  var seleccionGenero = document.querySelector("#titulo-genero");
-  var urlImg = "https://image.tmdb.org/t/p/original" + informacion.poster_path;
-  var serie = informacion.genre_ids;
-  var listaSeries = ""
-  for (var i = 0; i < serie.length; i++) {
-    listaSeries += '<a href="detalle.html">' + serie[i].name + '</a> ,'
-  }
+.then(function(informacion){
+  var generos = informacion.genres;
 
-  seleccionGenero.innerHTML = `
-  <h2>${titulo}</h2>
-  <img src="${urlImg}" width="250">
-  <p>Lenguaje original: ${lenguaje} </p>
-  <p>Fecha de estreno: ${fechaEstreno} </p>
-  <p>Género: ${listaGenero}</p>
-  <p>${descripcion}</p>
-`;
+  for (var i =0; i<=1; i++) {
+  nombreGenero.innerHTML += `
+  <h2>${generos[i].name}</h2>
+  `
+}
 })
-
 .catch(function(error) {
  console.log("Error: " + error);
+})
 })
