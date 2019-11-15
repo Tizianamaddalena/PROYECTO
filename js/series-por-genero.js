34f2f1f02f019e2e1340b56d194a8de7c3c55627
Nico <riccitelline@gmail.com>
2019-11-15 11:13:58 -0300

    PAGINA DE SERIES

--- js/series-por-genero.js
+++ js/series-por-genero.js
@@ -4,8 +4,8 @@ var urlParams = new URLSearchParams(location.search);
 var idGenero = urlParams.get('genre_ids');
 var nombreGenero = document.querySelector('#titulo-genero')
 
-
 var api_key = "60be6317b0012697cb25d6ce427de920"
+
 fetch("https://api.themoviedb.org/3/discover/tv?api_key="+api_key+"&language=en-US&sort_by=popularity.desc&page=1&timezone=America%2FNew_York&include_null_first_air_dates=false")
 .then(function(response){
   return response.json()
@@ -23,3 +23,4 @@ console.log(information);
  console.log("Error: " + error);
 })
 })
+//pagina de generos por series
