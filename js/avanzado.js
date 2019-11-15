0466dd7142a034a039efd3f581df9eeeb95f2e33
Tiziana <tizimb@gmail.com>
2019-11-15 11:25:36 -0300

    sigo con buscador avanzado GENEROS hecho

--- js/avanzado.js
+++ js/avanzado.js
@@ -1,20 +1,52 @@
 window.addEventListener('load',function(){
 //
 
+var searchParams = new URLSearchParams(window.location.search);
+var genre = searchParams.get('genre')
+var withoutG = searchParams.get('withoutGenres')
+var date = searchParams.get('date')
+var order = searchParams.get('order')
 
 
-var api_key= "60be6317b0012697cb25d6ce427de920"
-fetch("https://api.themoviedb.org/3/discover/tv?api_key=" + api_key + "&language=en-US&sort_by=popularity.desc&page=1&timezone=America%2FNew_York&include_null_first_air_dates=false")
+
+//var api_key= "60be6317b0012697cb25d6ce427de920"
+//fetch("https://api.themoviedb.org/3/discover/tv?api_key=" + api_key + "&language=en-US&sort_by=ORDEN&first_air_date_year=FECHA&page=1&with_genres=GENRE&without_genres=WITHOUT")
+fetch("https://api.themoviedb.org/3/discover/tv?api_key=60be6317b0012697cb25d6ce427de920&language=en-US&sort_by=popularity.desc")
 .then(function(response) {
  return response.json()
 })
 .then(function(information) {
     console.log(information);
+    var elegirGenero= document.querySelector("#genero-elegido");
 
-    var elegir-genero= document.querySelector("#genero-elegido");
 
 })
 
 .catch(function(error) {
  console.log("Error: " + error);
+})
+})
+
+
+// SELECCION DE GENERO
+var listaGeneros = document.querySelector('#genero-elegido');
+
+var api_key = "60be6317b0012697cb25d6ce427de920"
+fetch("https://api.themoviedb.org/3/genre/tv/list?api_key="+api_key+"&page=1")
+.then(function(response) {
+ return response.json()
+})
+.then(function(informacion){
+console.log(informacion);
+   var generos = informacion.genres;
+
+   for (var i = 0; i <= 16;i++) {
+     listaGeneros.innerHTML += `
+      <option>${generos[i].name}</option>
+      `
+    }
+  })
+
+.catch(function(error){
+  console.log("Error: " + error);
 })
