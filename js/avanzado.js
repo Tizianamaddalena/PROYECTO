497cc891324a004236904572415448c3913dfa5f
Tiziana <tizimb@gmail.com>
2019-11-15 09:42:25 -0300

    no me salio nada

--- js/avanzado.js
+++ js/avanzado.js
@@ -1,26 +1,20 @@
 window.addEventListener('load',function(){
+//
 
- var api = "https://api.themoviedb.org/3/discover/tv?api_key=60be6317b0012697cb25d6ce427de920&language=en-US&sort_by=popularity.desc&page=1&timezone=America%2FNew_York&include_null_first_air_dates=false"
-var listaGeneros = document.querySelector('genero-elegido');
 
-fetch(api)
+
+var api_key= "60be6317b0012697cb25d6ce427de920"
+fetch("https://api.themoviedb.org/3/discover/tv?api_key=" + api_key + "&language=en-US&sort_by=popularity.desc&page=1&timezone=America%2FNew_York&include_null_first_air_dates=false")
 .then(function(response) {
  return response.json()
 })
 .then(function(information) {
-console.log(information);
+    console.log(information);
 
-var generos = information.with_genres;
-for (var i = 0; i <= 15; i++) {
-  listaGeneros.innerHTML += `
-     <li><a href="series-por-genero.html?genero=${generos[i].id}">${generos[i].name}</a></li>
- `
- }
-
-
+    var elegir-genero= document.querySelector("#genero-elegido");
 
 })
+
 .catch(function(error) {
  console.log("Error: " + error);
-})
 })
