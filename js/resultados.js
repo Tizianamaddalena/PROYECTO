8e7bf488b85895cc558cc17e6159436d7fc52906
Tiziana <tizimb@gmail.com>
2019-11-21 14:20:04 -0300

    CAMBIOS

--- js/resultados.js
+++ js/resultados.js
@@ -1,5 +1,4 @@
 var parametrosURL = new URLSearchParams(location.search);
-
 var palabraBusqueda = parametrosURL.get("palabra_de_busqueda");
 
 console.log(palabraBusqueda);
@@ -8,7 +7,6 @@ console.log(palabraBusqueda);
 
 
 var api_key = "60be6317b0012697cb25d6ce427de920";
-
 fetch("https://api.themoviedb.org/3/search/tv?api_key=" + api_key + "&language=en-US&page=1&query=" + palabraBusqueda)
   .then(function (response) {
     return response.json();
