dac19e71aa46ed08045bd790ad1e0abd8b71e6f0
rosariorodriguezm <rrodriguezmontenegro@udesa.edu.ar>
2019-11-21 22:44:36 -0300

    favs

--- js/favoritoResultado.js
+++ js/favoritoResultado.js
@@ -6,8 +6,9 @@ window.onload= function () {
     var seriesFavoritas = JSON.parse (recuperacionStorage);
     console.log(seriesFavoritas);
 
+
     for (var i = 0; i < seriesFavoritas.length; i++) {
-      fetch("https://api.themoviedb.org/3/tv/" + id + "?api_key="+api_key)
+      fetch("https://api.themoviedb.org/3/tv/" + seriesFavoritas + "?api_key="+api_key)
 
 
       .then(function(response) {
@@ -17,38 +18,17 @@ window.onload= function () {
       .then(function(information) {
           console.log(information);
          var detalleSerie = document.querySelector("#favs");
-         var urlImg =  "https://image.tmdb.org/t/p/original" + information.poster_path;
-         // var titulo = information.name
-         // var descripcion = information.overview;
-         // var lenguaje = information.original_language
-         // var fechaEstreno = information.first_air_date
-         // var genero= information.genres
-         // var listaGenero = ""
-         // var
+         var posterURL =  "https://image.tmdb.org/t/p/original" + information.poster_path;
+         var titulo = information.name
 
+        
          serie = '<li  class="uk-animation-toggle" tabindex="0" >'
-         serie += '<a href="detalle.html?id=' + i + '">'
+         serie += '<a href="detalle.html?id=' + seriesFavoritas + '">'
          serie +=   '<img class="uk-animation-fade" src="'+posterURL+'" alt="">'
          serie +=   '<div class="uk-position-center uk-panel"><h2 class="">'+'</h2></div>'
          serie += '</a>'
          serie += '</li>'
-         // detalleSerie.innerHTML = `
-         //    <h2>${titulo}</h2>
-         //    <div id="foto_info">
-         //    <section class="info_serie">
-         //      <img src="${urlImg}" width="250">
-         //    </section>
-         //    <!-- <section class="info_serie">
-         //      <p>Lenguaje original: ${lenguaje} </p>
-         //      <p>Fecha de estreno: ${fechaEstreno} </p>
-         //      <p>Género: ${listaGenero}</p>
-         //
-         //      <p>${descripcion}</p>
-         //    </section> -->
-         //    </div>
-         //
-         //    `
-
+         detalleSerie.innerHTML += serie
 
       })
 
